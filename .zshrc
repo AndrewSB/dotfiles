0256003ad71698886ed14419d133f17b80bea540
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-16 15:50:26 -0700

    work quite a bit on the install script

--- .zshrc
+++ .zshrc
@@ -130,4 +130,10 @@ auto-ls () {
 zle -N accept-line auto-ls
 zle -N other-widget auto-ls
 
+wifipass() {
+	ssid = `/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`
+	
+	security find-generic-password -ga ssid
+}
+
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
--- install.swift
+++ install.swift
@@ -1,18 +1,56 @@
+#!/usr/bin/env xcrun swift
+/*
+// Here's what you're going to be installing
+*/
+enum CommandLineTool {
+    case Homebrew(tool: HomebrewTool)
+    case Cask(app: CaskApp)
+}
+enum HomebrewTool: String {
+    case Carthage =     "carthage"
+    case GitHub =       "hub"
+    case Node =         "node"
+    case Watchman =     "watchman"
+    case Cask =         "caskroom/cask/brew-cask"
+    
+    static let allValues = [Carthage, GitHub, Node, Watchman, Cask]
+}
+enum CaskApp: String {
+    case GoogleChrome = "google-chrome"
+    case Atom =         "atom"
+    case SkalaColor =   "colorpicker-skalacolor"
+    case Fabric =       "fabric"
+    case GitHub =       "github"
+    case Hermes =       "hermes"
+    case Sketch =       "sketch"
+    case Spotify =      "spotify"
+    case SublimeText =  "sublime-text"
+    case Transmission = "transmission"
+    
+    static let allValues = [GoogleChrome, Atom, SkalaColor, Fabric, GitHub, Hermes, Sketch, Spotify,SublimeText, Transmission]
+}
+enum Gem: String {
+    case CocoaPods =    "cocoapods"
+    case Synx =         "synx"
+    
+    static let allValues = [CocoaPods, Synx]
+}
+
+
 import Cocoa
 
 func main() {
     welcome()
     checkXCodeInstallation()
-    
-    println("escalting userPrivellages")
-    NSAppleScript(source: "do shell script \"sudo whatever\" with administrator privileges")!.executeAndReturnError(nil)
-    println("escalted")
+    installCommandLineTools()
+    shell("sudo", "gem", "install", "lol")
 }
+
 
 func welcome() {
     println("Yo yo")
-    println("Welcome to the superflous swift dotfile installtion script that Andrew wrote a day of age")
-    println("We're going to start out by making sure Xcode is installed, then install a bunch of command line tools, applications, and finally setup your dotfiles.")
+    println("Welcome to the superflous swift dotfile installtion script that Andrew wrote \nsome day of age")
+    println("We're going to start out by making sure Xcode is installed, then install a \nbunch of command line tools, applications, and finally setup your dotfiles.\n")
     let userStartResponse = promptUserInput("Sound good? (👍 /👎 )")
     if userStartResponse.rangeOfString("👍 ") == nil {
         println("Aight. We won't install")
@@ -27,15 +65,38 @@ func checkXCodeInstallation() {
         println("On the other hand, you cloned this without getting git on your machine. Quite boss")
         println("Go hit up the app store and get Xcode. You're going to need it")
         exit(0)
+    } else {
+        println("Looks like you have Xcode installed, thats good!")
     }
 }
 
 func installCommandLineTools() {
+    println("Installing Homebrew")
     let homeBrewInstallScript = shell("curl", "-fsSL", "https://raw.githubusercontent.com/Homebrew/install/master/install")
-    let lol = shell("ruby", "-e", homeBrewInstallScript)
+
+    for tool in HomebrewTool.allValues {
+        shell("brew", "install", tool.rawValue)
+    }
+    println("installed all homebrew tools")
+    
+    for app in CaskApp.allValues {
+        shell("brew", "cask", "install", app.rawValue)
+    }
+    println("installed all cask apps")
+    
+    for gem in Gem.allValues {
+        shell("sudo", "gem", "install", gem.rawValue)
+    }
+}
+
+func installDotfiles() {
+    shell("cp", ".zshrc", "~/.zshrc")
+    shell("cp", ".gitconfig", "~/.gitconfig")
 }
 
 // LOW LEVEL
+
+
 func shell(args: String...) -> String {
     let task = NSTask()
     let pipe = NSPipe()
