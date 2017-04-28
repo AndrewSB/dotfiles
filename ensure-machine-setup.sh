106c4364b01c6db5091268c8ca1ef40b5db23e36
Andrew Breckenridge <asbreckenridge@me.com>
2017-04-28 16:14:19 -0700

    add archey to brew dependencies in setup

--- ensure-machine-setup.sh
+++ ensure-machine-setup.sh
@@ -4,7 +4,7 @@
 # It 
 # 	1. makes sure you have XCode's command line tools
 # 	2. Gets homebrew and installs
-brew_dependencies=("swiftgen" "swiftlint" "carthage" "trash" "hub" "node")
+brew_dependencies=("swiftgen" "swiftlint" "carthage" "trash" "hub" "node" "archey")
 # 	3. installs oh-my-zsh
 #	4. makes sure you have ruby greater than
 min_ruby_version=2.3
