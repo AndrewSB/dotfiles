52512c7d1750c1c487a288ea2872bbc44d8107d7
Andrew Breckenridge <asbreckenridge@me.com>
2020-04-10 14:40:12 -0700

    ensure-machine-setup.sh: Update to actually be useful again

--- scripts/ensure-machine-setup.sh
+++ scripts/ensure-machine-setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
 # This script ensures that a machine is setup for my usual development work.
-# It 
+# It
 # 	1. makes sure you have XCode's command line tools
 # 	2. Gets homebrew and installs
 ios_brew_dependencies=("swiftgen" "swiftlint" "carthage")
-brew_dependencies=("trash" "hub" "node" "scmpuff")
+brew_dependencies=("trash" "hub" "node" "scmpuff" "neovim")
+cask_dependencies=("docker" "tripmode" "figmadaemon" "visual-studio-code")
 # 	3. installs zim
 #	4. makes sure you have ruby greater than
 min_ruby_version=2.3
@@ -18,7 +19,27 @@ source ./machine-setup-functions.sh
 can_i_xcode
 can_i_brew
 can_i_brew_deps
-can_i_zim
+can_i_cask_deps
 can_i_ruby
-can_i_gem
+if [ ! ${#gem_dependencies[@]} -eq 0 ]; then # skip installing gems if empty
+    can_i_gem
+fi
+
+
+echo ""
 echo "All done 🎉"
+echo "Don't forget to run ./copy to get the dotfiles, and ./setup-macos.sh (WHICH WILL KILLALL TERMINAL)."
+read -p "Would you like to run them here? (y/n)" answer
+case ${answer:0:1} in
+  y|Y )
+    set -e
+    echo "running copy"
+    ./copy
+    echo "running setup-macos.sh, which will cause Terminal to quit"
+    ./setup-macos.sh
+    set +e
+  ;;
+  * )
+    echo "alright, not running 🙃"
+  ;;
+esac
--- scripts/machine-setup-functions.sh
+++ scripts/machine-setup-functions.sh
@@ -4,19 +4,19 @@
 # http://stackoverflow.com/a/17841619
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-function fire_echo { echo "🔥 " $@; }
+function fire_echo { echo "🔥" $@; }
 
 function can_i_xcode() {
 	xcode-select -p >&/dev/null # this checks if xcode command line tools are installed (http://stackoverflow.com/a/15371967)
-	if [ $? -eq 0 ]; then 
+	if [ $? -eq 0 ]; then
 		# if xcode-select exited with 0, then you have command line tools
 		if [ ! -d "/Applications/Xcode.app" ]; then
 			fire_echo "It looks like you have Xcode's command line tools installed, but no XCode"
 			fire_echo "You might want to get Xcode from the Mac App Store"
 		else
-			fire_echo "You have XCode 🎉"
+			fire_echo "You have Xcode 🎉"
 		fi
-	else 
+	else
 		fire_echo "It looks as though you don't have XCode command line tools."
 		fire_echo "You can either download & open Xcode, or run xcode-select --install"
 		fire_echo "Try running this again after you've done that."
@@ -25,7 +25,7 @@ function can_i_xcode() {
 }
 
 function can_i_oh_my_zsh() {
-	set -e 
+	set -e
 		if ! [ -d ~/.oh-my-zsh ]; then
 			fire_echo "You don't have oh-my-zsh, let's install it"
 			sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -50,18 +50,29 @@ function can_i_brew() {
 		fire_echo "You don't have homebrew, we're going to install it for you"
 		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
-	set +e 
+	set +e
 }
 
 function can_i_brew_deps() {
-	fire_echo "You have brew, we're going to install `join_by , "${brew_dependencies[@]}"` for you, if you don't have them"
-	for dependency in "${brew_dependencies[@]}"; do
-		if ! which "${dependency}" >/dev/null; then
-			fire_echo "Downloading ${dependency}"
-			set -e; /usr/local/bin/brew install "${dependency}"; set +e
-		fi
-	done
-	fire_echo "Done installing brew dependencies! You have `join_by , "${brew_dependencies[@]}"` all installed"
+	fire_echo "You have brew, we're going to install some packages"
+	read -p "Would you like to also install iOS packages (`join_by , "${ios_brew_dependencies[@]}"`) (y/n)? " answer
+	case ${answer:0:1} in
+		y|Y )
+			BREW_INSTALLS=("${brew_dependencies[@]}" "${ios_brew_dependencies[@]}")
+		;;
+		* )
+			BREW_INSTALLS=("${brew_dependencies[@]}")
+		;;
+	esac
+	fire_echo "Installing `join_by , "${BREW_INSTALLS[@]}"`."
+	set -e; /usr/local/bin/brew install ${BREW_INSTALLS[@]}; set +e;
+}
+
+function can_i_cask_deps() {
+	fire_echo "Going to install brew casks now: `join_by , "${cask_dependencies[@]}"`"
+	set -e
+	/usr/local/bin/brew cask install ${cask_dependencies[*]}
+	set +e;
 }
 
 function can_i_ruby() {
