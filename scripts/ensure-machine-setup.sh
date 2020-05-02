444835f50056903818dfc89790f120101cd2298d
Andrew Breckenridge <asbreckenridge@me.com>
2020-05-01 17:34:02 -0700

    scripts: Update them to be callable from any dir, add casks & nvm

--- scripts/ensure-machine-setup.sh
+++ scripts/ensure-machine-setup.sh
@@ -2,28 +2,22 @@
 #
 # This script ensures that a machine is setup for my usual development work.
 # It
-# 	1. makes sure you have XCode's command line tools
+# 	1. makes sure you have Xcode's command line tools
 # 	2. Gets homebrew and installs
-ios_brew_dependencies=("swiftgen" "swiftlint" "carthage")
-brew_dependencies=("trash" "hub" "node" "scmpuff" "neovim")
-cask_dependencies=("docker" "tripmode" "figmadaemon" "visual-studio-code")
-# 	3. installs zim
-#	4. makes sure you have ruby greater than
-min_ruby_version=2.3
-#	   and installs ruby through rvm for you if you have a lower version
-#	5. installs some dependencies through gem
-gem_dependencies=()
+ios_brew_dependencies=("swiftgen" "swiftlint" "carthage") # optionally, asks in a prompt
+brew_dependencies=("trash" "hub" "gh" "scmpuff" "neovim")
+cask_dependencies=("docker" "tripmode" "figmadaemon" "visual-studio-code" "alt-tab" "protonvpn" "vlc" "github")
+# and installs nvm
 
-source ./machine-setup-functions.sh
+DIRNAME=`dirname "$0"`
+
+source $DIRNAME/machine-setup-functions.sh
 
 can_i_xcode
 can_i_brew
 can_i_brew_deps
 can_i_cask_deps
-can_i_ruby
-if [ ! ${#gem_dependencies[@]} -eq 0 ]; then # skip installing gems if empty
-    can_i_gem
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 
 echo ""
