d8815ff0e381f6b730ac972508ab58945b0f763f
Andrew Breckenridge <asbreckenridge@me.com>
2020-05-08 21:01:58 -0700

    profile: make profile cross platform by sourcing an profile.

--- profile
+++ profile
@@ -23,18 +23,16 @@ if [[ -z "$LANG" ]]; then
 fi
 
 # aliases
-alias rm="trash" # lets never actually rm, that scares me. To actually rm, run `\rm`
-alias xc="open *.{xcworkspace,xcodeproj}(N)"
-alias synx="synx --no-sort-by-name"
-alias b="brew"
-alias networkpopup="open /System/Library/CoreServices/Captive\ Network\ Assistant.app"
 alias make="CDPATH="" /usr/bin/make $@" #override the CDPATH while `make`ing. It sometimes causes [issues](https://github.com/thoughtbot/capybara-webkit/issues/56)
-alias arc-upstream="~/Developer/arcanist/bin/arc"
 alias e="scmpuff expand"
-alias fuckwifi="networksetup -serairportpower airport off; networksetup -setairportpower airport on"
 
 # utilities
 eval "$(scmpuff init -s)"
 
 # source a .secrets file, if it exists, that stores things I can't store on github
 [[ -f ~/.secrets ]] && source ~/.secrets
+
+# source any platform specific profiles
+for PLATFORM_PROFILE in ".profile.macos" ".profile.ubuntu"; do
+  [[ -f ~/$PLATFORM_PROFILE ]] && source ~/$PLATFORM_PROFILE
+done
--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# https://medium.com/@ssmak/how-to-fix-puppetteer-error-while-loading-shared-libraries-libx11-xcb-so-1-c1918b75acc3
+CHROME_DEPS=( "gconf-service" "libasound2" "libatk1.0-0" "libatk-bridge2.0-0" "libc6" "libcairo2" "libcups2" "libdbus-1-3" "libexpat1" "libfontconfig1" "libgcc1" "libgconf-2-4" "libgdk-pixbuf2.0-0" "libglib2.0-0" "libgtk-3-0" "libnspr4" "libpango-1.0-0" "libpangocairo-1.0-0" "libstdc++6" "libx11-6" "libx11-xcb1" "libxcb1" "libxcomposite1" "libxcursor1" "libxdamage1" "libxext6" "libxfixes3" "libxi6" "libxrandr2" "libxrender1" "libxss1" "libxtst6" "ca-certificates" "fonts-liberation" "libappindicator1" "libnss3" "lsb-release" "xdg-utils" "wget" )
+
+PACKAGES=( "mosh" "zsh" "neovim" )
+
+sudo apt-get update
+sudo apt-get install --yes "${CHROME_DEPS[@]}" "${PACKAGES[@]}"
+
 # switch to zsh
 sudo chsh -s /usr/bin/zsh asb
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# install scmpuff
+pushd "$(mktemp -d)"
+curl -L https://github.com/mroth/scmpuff/releases/download/v0.3.0/scmpuff_0.3.0_linux_x64.tar.gz | tar xvz
+sudo mv scmpuff /usr/local/bin/
+popd
