8e5bfa2b312777e801ff0f5abc96e56bc6d0a1f4
Andrew Breckenridge <asbreckenridge@me.com>
2020-09-24 02:58:26 -0700

    scripts/new-mac: nvm install and alias stable to default

--- scripts/new-mac.sh
+++ scripts/new-mac.sh
@@ -12,15 +12,17 @@ pushd dotfiles
 
 yes n | ./scripts/setup-macos.sh
 ./scripts/copy-macos
-source ./scripts/ensure-machine-setup.sh # this gets the variables like `brew_dependencies` and `cask_dependencies`
 can_i_brew
 
 brew_dependencies=("trash" "gh" "scmpuff" "neovim" "swiftlint" "MisterTea/et/et" "git-absorb" "go" "watchman")
 cask_dependencies=("docker" "tripmode" "visual-studio-code" "alt-tab" "protonvpn" "vlc" "github" "tandem")
-
 yes n | can_i_brew_deps
 can_i_cask_deps
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install stable
+nvm alias default stable
 
 # aka popd --all (https://unix.stackexchange.com/a/353361)
 pushd -0 && dirs -c
