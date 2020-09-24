591dcbdbe7c10506bb693ff23eeb610156f410a8
Andrew Breckenridge <asbreckenridge@me.com>
2020-09-24 02:56:33 -0700

    scripts/ensure-machine-setup.sh replace with new-mac.sh

--- scripts/new-mac.sh
+++ scripts/new-mac.sh
@@ -14,6 +14,10 @@ yes n | ./scripts/setup-macos.sh
 ./scripts/copy-macos
 source ./scripts/ensure-machine-setup.sh # this gets the variables like `brew_dependencies` and `cask_dependencies`
 can_i_brew
+
+brew_dependencies=("trash" "gh" "scmpuff" "neovim" "swiftlint" "MisterTea/et/et" "git-absorb" "go" "watchman")
+cask_dependencies=("docker" "tripmode" "visual-studio-code" "alt-tab" "protonvpn" "vlc" "github" "tandem")
+
 yes n | can_i_brew_deps
 can_i_cask_deps
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
