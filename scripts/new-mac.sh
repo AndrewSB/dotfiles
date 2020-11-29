78413cee1fd5fdd74fc5f72a12b934e292c5e764
Andrew Breckenridge <asbreckenridge@me.com>
2020-11-29 13:46:32 -0800

    new-mac: hopefully fix nvm installation

--- scripts/new-mac.sh
+++ scripts/new-mac.sh
@@ -42,8 +42,8 @@ yes | can_i_brew_deps "${brew_dependencies[@]}"
 can_i_cask_deps "${cask_dependencies[@]}"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-# TODO: this didn't work for some reason, debug next time
 # shellcheck source=/dev/null
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install stable
 nvm alias default stable
