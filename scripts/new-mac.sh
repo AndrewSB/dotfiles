c1b6be41088f8bf15b075056e4476bbbe7440275
Andrew Breckenridge <asbreckenridge@me.com>
2022-04-25 13:11:25 -0700

    new-mac: remove xcode-select --install in favor of brew install side-effect

--- scripts/new-mac.sh
+++ scripts/new-mac.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-
 # runnable as `curl -L andrew.energy/newmac | bash`
 
-#
 # Current workflow
-# run script 
+# run script
 # clone a bunch of repositories (might be nice to have a Working.bundle that can clone automatically
-# most painful: sign into all services
-
-pushd ~
-mkdir Developer
-
-xcode-select --install
-sudo xcodebuild -license accept
+# most painful: sign into all services in Safari with 2FAC
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install gh
 gh auth login
+
+pushd ~
+mkdir Developer
 
 pushd ~/Developer
 gh repo clone AndrewSB/dotfiles
