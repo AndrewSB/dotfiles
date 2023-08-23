fb27cc5dda9aef30cc1fd165bbb01dc906871f66
Andrew Breckenridge <asbreckenridge@me.com>
2023-08-23 09:56:32 -0700

    script/new-mac.sh: refactor

--- script/new-mac.sh
+++ script/new-mac.sh
@@ -2,18 +2,20 @@
 # runnable as `curl -L andrew.energy/newmac | bash`
 
 sudo echo
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
+# Install Homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)" || eval "$(/usr/local/bin/brew shellenv)"
+
+# Install Mackup
 brew install mackup
 
 # creates ~/.mackup.cfg and writes the following to it:
 # [storage]
 # engine = icloud
 rm ~/.mackup.cfg
-touch ~/.mackup.cfg
-echo "[storage]" >> ~/.mackup.cfg
-echo "engine = icloud" >> ~/.mackup.cfg
+echo "[storage]" > "$HOME/.mackup.cfg"
+echo "engine = icloud" >> "$HOME/.mackup.cfg"
 
 mackup restore -f
 
@@ -22,13 +24,13 @@ brew install gh
 mkdir Developer
 pushd ~/Developer || exit
 
-mkdir job
-
 gh repo clone AndrewSB/dotfiles
 pushd dotfiles || exit
 
 set -e
 
 brew bundle
+
+popd || exit
 
 xcodes install --latest
